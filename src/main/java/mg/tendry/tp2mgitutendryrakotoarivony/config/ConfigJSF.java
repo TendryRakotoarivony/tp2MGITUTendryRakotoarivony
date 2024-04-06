bc0cddcbfcaea61eaa832f5f2fccc341f8a1206c
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-04-06 20:54:39 +0300

    Init

---
+++ src/main/java/mg/tendry/tp2mgitutendryrakotoarivony/config/ConfigJSF.java
@@ -0,0 +1,11 @@
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+/**
+ *
+ * @author Tendry Arivony
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+
+}
